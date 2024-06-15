9a3cedf832f7cab34c1e6a30e018290f52f941eb
den <142715769+woob123@users.noreply.github.com>
2024-06-15 22:47:41 +0300

    Ran data gen

--- src/main/java/net/woob123/testmod/datagen/DataGenerators.java
+++ src/main/java/net/woob123/testmod/datagen/DataGenerators.java
@@ -4,13 +4,16 @@ package net.woob123.testmod.datagen;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
 import net.woob123.testmod.TestMod;
 
 import java.util.concurrent.CompletableFuture;
 
+@EventBusSubscriber(modid = TestMod.MOD_ID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class DataGenerators {
     @SubscribeEvent
     public static void gatherData(GatherDataEvent event){
