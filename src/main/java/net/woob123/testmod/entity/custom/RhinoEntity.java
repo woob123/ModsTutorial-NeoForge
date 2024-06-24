c087a116742e7d4eb88bc8eab2641cd20bd77831
den <142715769+woob123@users.noreply.github.com>
2024-06-24 11:46:39 +0300

    Tutorial 27 -> Custom Attack Animations (no work need to come back)

--- src/main/java/net/woob123/testmod/entity/custom/RhinoEntity.java
+++ src/main/java/net/woob123/testmod/entity/custom/RhinoEntity.java
@@ -1,5 +1,9 @@
 package net.woob123.testmod.entity.custom;
 
+import net.minecraft.network.syncher.EntityDataAccessor;
+import net.minecraft.network.syncher.EntityDataSerializer;
+import net.minecraft.network.syncher.EntityDataSerializers;
+import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.AgeableMob;
 import net.minecraft.world.entity.AnimationState;
@@ -8,6 +12,7 @@ import net.minecraft.world.entity.Pose;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.*;
+import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
 import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
@@ -15,16 +20,21 @@ import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.Level;
 import net.woob123.testmod.entity.ModEntities;
+import net.woob123.testmod.entity.ai.RhinoAttackGoal;
 import net.woob123.testmod.item.ModItems;
 import org.jetbrains.annotations.Nullable;
 
 public class RhinoEntity extends Animal {
+    private static final EntityDataAccessor<Boolean> ATTACKING = SynchedEntityData.defineId(RhinoEntity.class, EntityDataSerializers.BOOLEAN);
     public RhinoEntity(EntityType<? extends Animal> pEntityType, Level pLevel) {
         super(pEntityType, pLevel);
     }
 
     public final AnimationState idleAnimationState = new AnimationState();
     private int idleAnimationTimeout = 0;
+
+    public final AnimationState attackAnimationState = new AnimationState();
+    public int attackAnimationTimeout = 0;
 
 
     @Override
@@ -43,6 +53,17 @@ public class RhinoEntity extends Animal {
         } else {
             --this.idleAnimationTimeout;
         }
+
+        if(this.isAttacking() && attackAnimationTimeout <= 0) {
+            attackAnimationTimeout = 80;
+            attackAnimationState.start(this.tickCount);
+        } else {
+            --this.attackAnimationTimeout;
+        }
+
+        if(!this.isAttacking()) {
+            attackAnimationState.stop();
+        }
     }
 
     @Override
@@ -57,11 +78,27 @@ public class RhinoEntity extends Animal {
         this.walkAnimation.update(f, 0.2f);
     }
 
+    public void setAttacking(boolean attacking){
+        this.entityData.set(ATTACKING ,attacking);
+    }
+
+    public boolean isAttacking(){
+        return this.entityData.get(ATTACKING);
+    }
+
+    @Override
+    protected void defineSynchedData(SynchedEntityData.Builder pBuilder) {
+        super.defineSynchedData(pBuilder);
+        pBuilder.define(ATTACKING, false);
+    }
+
     @Override
     protected void registerGoals() {
         this.goalSelector.addGoal(0, new FloatGoal(this));
-        this.goalSelector.addGoal(1, new BreedGoal(this, 1.5D));
-        this.goalSelector.addGoal(1, new TemptGoal(this, 1.5D, Ingredient.of(ModItems.CORN.get()), false));
+        this.goalSelector.addGoal(1, new RhinoAttackGoal(this, 1.0D, true));
+
+        this.goalSelector.addGoal(2, new BreedGoal(this, 1.5D));
+        this.goalSelector.addGoal(2, new TemptGoal(this, 1.5D, Ingredient.of(ModItems.CORN.get()), false));
 
         this.goalSelector.addGoal(3, new FollowParentGoal(this, 1.1D));
 
@@ -69,7 +106,7 @@ public class RhinoEntity extends Animal {
         this.goalSelector.addGoal(5, new LookAtPlayerGoal(this, Player.class, 3f));
         this.goalSelector.addGoal(6, new RandomLookAroundGoal(this));
 
-
+        this.targetSelector.addGoal(1, new HurtByTargetGoal(this));
     }
 
     public static AttributeSupplier.Builder createAttributes(){
